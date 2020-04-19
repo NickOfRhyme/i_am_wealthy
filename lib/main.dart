c8f90db2114e0e50f62b673686f3e3c342e0240b
NickOfRhyme <nhwoolrich@gmail.com>
2020-04-19 08:47:15 +0100

    add placeholder image

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,17 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("I am Wealthy"),
-          centerTitle: true,
-          backgroundColor: Colors.teal[600],
-        ),
-        backgroundColor: Colors.teal[100],
-      ),
+          appBar: AppBar(
+            title: Text('I am Wealthy'),
+            centerTitle: true,
+            backgroundColor: Colors.teal[600],
+          ),
+          backgroundColor: Colors.teal[100],
+          body: Center(
+              child: Image(
+            image: NetworkImage(
+                'https://images.pexels.com/photos/414171/pexels-photo-414171.jpeg?cs=srgb&dl=adventure-calm-clouds-dawn-414171.jpg'),
+          ))),
     ),
   );
 }
