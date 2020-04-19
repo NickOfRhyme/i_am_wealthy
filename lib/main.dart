ec5cad64e2e078c399ddc2460aea8304d92574e0
NickOfRhyme <nhwoolrich@gmail.com>
2020-04-19 12:12:22 +0100

    replaced placeholder image and icons

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text('I am Wealthy'),
-            centerTitle: true,
-            backgroundColor: Colors.teal[600],
-          ),
-          backgroundColor: Colors.teal[100],
-          body: Center(
-              child: Image(
-            image: NetworkImage(
-                'https://images.pexels.com/photos/414171/pexels-photo-414171.jpeg?cs=srgb&dl=adventure-calm-clouds-dawn-414171.jpg'),
-          ))),
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('I Am Wealthy'),
+        centerTitle: true,
+        backgroundColor: Colors.teal[600],
+      ),
+      backgroundColor: Colors.teal[100],
+      body: Center(
+        child: Image(
+          image: AssetImage('images/make_it_rain.png'),
+        ),
+      ),
     ),
-  );
+  ));
 }
