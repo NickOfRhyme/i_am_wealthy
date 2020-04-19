ca9c4f12bbcc0a0575891bf4bbc89503ada61a74
NickOfRhyme <nhwoolrich@gmail.com>
2020-04-19 08:38:42 +0100

    set up initial scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,16 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("I am Wealthy"),
+          centerTitle: true,
+          backgroundColor: Colors.teal[600],
+        ),
+        backgroundColor: Colors.teal[100],
+      ),
+    ),
+  );
 }
